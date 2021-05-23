ca3117cf1a2399ac16f418d0dfa4c66a76944fe1
NABIN KARKI <nabin0433@gmail.com>
2021-05-23 18:51:52 +0545

    RE-STRIPE_SIGNING_SECRET add

--- src/pages/api/webhook.js
+++ src/pages/api/webhook.js
@@ -11,7 +11,7 @@ const app = !admin.apps.length
 
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
-const key = STRIPE_SIGNING_SECRET;
+const key = process.env.STRIPE_SIGNING_SECRET;
 
 const fullfillOrder = async (session) => {
   return await app
